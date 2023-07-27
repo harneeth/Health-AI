b0e1832df11de8063b947d021f4c2e9fd911d265
harneeth <132078484+harneeth@users.noreply.github.com>
2023-07-27 15:33:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,458 @@
+var symptoms = [];
+
+const button0 = document.getElementById("0");
+button0.addEventListener("click", function() {addtolist("0")});
+
+const button1 = document.getElementById("1");
+button1.addEventListener("click", function() {addtolist("1")});
+
+const button2 = document.getElementById("2");
+button2.addEventListener("click", function() {addtolist("2")});
+
+const button3 = document.getElementById("3");
+button3.addEventListener("click", function() {addtolist("3")});
+
+const button4 = document.getElementById("4");
+button4.addEventListener("click", function() {addtolist("4")});
+
+const button5 = document.getElementById("5");
+button5.addEventListener("click", function() {addtolist("5")});
+
+const button6 = document.getElementById("6");
+button6.addEventListener("click", function() {addtolist("6")});
+
+const button7 = document.getElementById("7");
+button7.addEventListener("click", function() {addtolist("7")});
+
+const button8 = document.getElementById("8");
+button8.addEventListener("click", function() {addtolist("8")});
+
+const button9 = document.getElementById("9");
+button9.addEventListener("click", function() {addtolist("9")});
+
+const button10 = document.getElementById("10");
+button10.addEventListener("click", function() {addtolist("10")});
+
+const button11 = document.getElementById("11");
+button11.addEventListener("click", function() {addtolist("11")});
+
+const button12 = document.getElementById("12");
+button12.addEventListener("click", function() {addtolist("12")});
+
+const button13 = document.getElementById("13");
+button13.addEventListener("click", function() {addtolist("13")});
+
+const button14 = document.getElementById("14");
+button14.addEventListener("click", function() {addtolist("14")});
+
+const button15 = document.getElementById("15");
+button15.addEventListener("click", function() {addtolist("15")});
+
+const button16 = document.getElementById("16");
+button16.addEventListener("click", function() {addtolist("16")});
+
+const button17 = document.getElementById("17");
+button17.addEventListener("click", function() {addtolist("17")});
+
+const button18 = document.getElementById("18");
+button18.addEventListener("click", function() {addtolist("18")});
+
+const button19 = document.getElementById("19");
+button19.addEventListener("click", function() {addtolist("19")});
+
+const button20 = document.getElementById("20");
+button20.addEventListener("click", function() {addtolist("20")});
+
+const button21 = document.getElementById("21");
+button21.addEventListener("click", function() {addtolist("21")});
+
+const button22 = document.getElementById("22");
+button22.addEventListener("click", function() {addtolist("22")});
+
+const button23 = document.getElementById("23");
+button23.addEventListener("click", function() {addtolist("23")});
+
+const button24 = document.getElementById("24");
+button24.addEventListener("click", function() {addtolist("24")});
+
+const button25 = document.getElementById("25");
+button25.addEventListener("click", function() {addtolist("25")});
+
+const button26 = document.getElementById("26");
+button26.addEventListener("click", function() {addtolist("26")});
+
+const button27 = document.getElementById("27");
+button27.addEventListener("click", function() {addtolist("27")});
+
+const button28 = document.getElementById("28");
+button28.addEventListener("click", function() {addtolist("28")});
+
+const button29 = document.getElementById("29");
+button29.addEventListener("click", function() {addtolist("29")});
+
+const button30 = document.getElementById("30");
+button30.addEventListener("click", function() {addtolist("30")});
+
+const button31 = document.getElementById("31");
+button31.addEventListener("click", function() {addtolist("31")});
+
+const button32 = document.getElementById("32");
+button32.addEventListener("click", function() {addtolist("32")});
+
+const button33 = document.getElementById("33");
+button33.addEventListener("click", function() {addtolist("33")});
+
+const button34 = document.getElementById("34");
+button34.addEventListener("click", function() {addtolist("34")});
+
+const button35 = document.getElementById("35");
+button35.addEventListener("click", function() {addtolist("35")});
+
+const button36 = document.getElementById("36");
+button36.addEventListener("click", function() {addtolist("36")});
+
+const button37 = document.getElementById("37");
+button37.addEventListener("click", function() {addtolist("37")});
+
+const button38 = document.getElementById("38");
+button38.addEventListener("click", function() {addtolist("38")});
+
+const button39 = document.getElementById("39");
+button39.addEventListener("click", function() {addtolist("39")});
+
+const button40 = document.getElementById("40");
+button40.addEventListener("click", function() {addtolist("40")});
+
+const button41 = document.getElementById("41");
+button41.addEventListener("click", function() {addtolist("41")});
+
+const button42 = document.getElementById("42");
+button42.addEventListener("click", function() {addtolist("42")});
+
+const button43 = document.getElementById("43");
+button43.addEventListener("click", function() {addtolist("43")});
+
+const button44 = document.getElementById("44");
+button44.addEventListener("click", function() {addtolist("44")});
+
+const button45 = document.getElementById("45");
+button45.addEventListener("click", function() {addtolist("45")});
+
+const button46 = document.getElementById("46");
+button46.addEventListener("click", function() {addtolist("46")});
+
+const button47 = document.getElementById("47");
+button47.addEventListener("click", function() {addtolist("47")});
+
+const button48 = document.getElementById("48");
+button48.addEventListener("click", function() {addtolist("48")});
+
+const button49 = document.getElementById("49");
+button49.addEventListener("click", function() {addtolist("49")});
+
+const button50 = document.getElementById("50");
+button50.addEventListener("click", function() {addtolist("50")});
+
+const button51 = document.getElementById("51");
+button51.addEventListener("click", function() {addtolist("51")});
+
+const button52 = document.getElementById("52");
+button52.addEventListener("click", function() {addtolist("52")});
+
+const button53 = document.getElementById("53");
+button53.addEventListener("click", function() {addtolist("53")});
+
+const button54 = document.getElementById("54");
+button54.addEventListener("click", function() {addtolist("54")});
+
+const button55 = document.getElementById("55");
+button55.addEventListener("click", function() {addtolist("55")});
+
+const button56 = document.getElementById("56");
+button56.addEventListener("click", function() {addtolist("56")});
+
+const button57 = document.getElementById("57");
+button57.addEventListener("click", function() {addtolist("57")});
+
+const button58 = document.getElementById("58");
+button58.addEventListener("click", function() {addtolist("58")});
+
+const button59 = document.getElementById("59");
+button59.addEventListener("click", function() {addtolist("59")});
+
+const button60 = document.getElementById("60");
+button60.addEventListener("click", function() {addtolist("60")});
+
+const button61 = document.getElementById("61");
+button61.addEventListener("click", function() {addtolist("61")});
+
+const button62 = document.getElementById("62");
+button62.addEventListener("click", function() {addtolist("62")});
+
+const button63 = document.getElementById("63");
+button63.addEventListener("click", function() {addtolist("63")});
+
+const button64 = document.getElementById("64");
+button64.addEventListener("click", function() {addtolist("64")});
+
+const button65 = document.getElementById("65");
+button65.addEventListener("click", function() {addtolist("65")});
+
+const button66 = document.getElementById("66");
+button66.addEventListener("click", function() {addtolist("66")});
+
+const button67 = document.getElementById("67");
+button67.addEventListener("click", function() {addtolist("67")});
+
+const button68 = document.getElementById("68");
+button68.addEventListener("click", function() {addtolist("68")});
+
+const button69 = document.getElementById("69");
+button69.addEventListener("click", function() {addtolist("69")});
+
+const button70 = document.getElementById("70");
+button70.addEventListener("click", function() {addtolist("70")});
+
+const button71 = document.getElementById("71");
+button71.addEventListener("click", function() {addtolist("71")});
+
+const button72 = document.getElementById("72");
+button72.addEventListener("click", function() {addtolist("72")});
+
+const button73 = document.getElementById("73");
+button73.addEventListener("click", function() {addtolist("73")});
+
+const button74 = document.getElementById("74");
+button74.addEventListener("click", function() {addtolist("74")});
+
+const button76 = document.getElementById("76");
+button76.addEventListener("click", function() {addtolist("76")});
+
+const button77 = document.getElementById("77");
+button77.addEventListener("click", function() {addtolist("77")});
+
+const button78 = document.getElementById("78");
+button78.addEventListener("click", function() {addtolist("78")});
+
+const button79 = document.getElementById("79");
+button79.addEventListener("click", function() {addtolist("79")});
+
+const button80 = document.getElementById("80");
+button80.addEventListener("click", function() {addtolist("80")});
+
+const button81 = document.getElementById("81");
+button81.addEventListener("click", function() {addtolist("81")});
+
+const button82 = document.getElementById("82");
+button82.addEventListener("click", function() {addtolist("82")});
+
+const button83 = document.getElementById("83");
+button83.addEventListener("click", function() {addtolist("83")});
+
+const button84 = document.getElementById("84");
+button84.addEventListener("click", function() {addtolist("84")});
+
+const button85 = document.getElementById("85");
+button85.addEventListener("click", function() {addtolist("85")});
+
+const button86 = document.getElementById("86");
+button86.addEventListener("click", function() {addtolist("86")});
+
+const button87 = document.getElementById("87");
+button87.addEventListener("click", function() {addtolist("87")});
+
+const button88 = document.getElementById("88");
+button88.addEventListener("click", function() {addtolist("88")});
+
+const button89 = document.getElementById("89");
+button89.addEventListener("click", function() {addtolist("89")});
+
+const button90 = document.getElementById("90");
+button90.addEventListener("click", function() {addtolist("90")});
+
+const button91 = document.getElementById("91");
+button91.addEventListener("click", function() {addtolist("91")});
+
+const button92 = document.getElementById("92");
+button92.addEventListener("click", function() {addtolist("92")});
+
+const button93 = document.getElementById("93");
+button93.addEventListener("click", function() {addtolist("93")});
+
+const button94 = document.getElementById("94");
+button94.addEventListener("click", function() {addtolist("94")});
+
+const button95 = document.getElementById("95");
+button95.addEventListener("click", function() {addtolist("95")});
+
+const button96 = document.getElementById("96");
+button96.addEventListener("click", function() {addtolist("96")});
+
+const button97 = document.getElementById("97");
+button97.addEventListener("click", function() {addtolist("97")});
+
+const button98 = document.getElementById("98");
+button98.addEventListener("click", function() {addtolist("98")});
+
+const button99 = document.getElementById("99");
+button99.addEventListener("click", function() {addtolist("99")});
+
+const button100 = document.getElementById("100");
+button100.addEventListener("click", function() {addtolist("100")});
+
+const button101 = document.getElementById("101");
+button101.addEventListener("click", function() {addtolist("101")});
+
+const button102 = document.getElementById("102");
+button102.addEventListener("click", function() {addtolist("102")});
+
+const button103 = document.getElementById("103");
+button103.addEventListener("click", function() {addtolist("103")});
+
+const button104 = document.getElementById("104");
+button104.addEventListener("click", function() {addtolist("104")});
+
+const button105 = document.getElementById("105");
+button105.addEventListener("click", function() {addtolist("105")});
+
+const button106 = document.getElementById("106");
+button106.addEventListener("click", function() {addtolist("106")});
+
+const button107 = document.getElementById("107");
+button107.addEventListener("click", function() {addtolist("107")});
+
+const button108 = document.getElementById("108");
+button108.addEventListener("click", function() {addtolist("108")});
+
+const button109 = document.getElementById("109");
+button109.addEventListener("click", function() {addtolist("109")});
+
+const button110 = document.getElementById("110");
+button110.addEventListener("click", function() {addtolist("110")});
+
+const button111 = document.getElementById("111");
+button111.addEventListener("click", function() {addtolist("111")});
+
+const button112 = document.getElementById("112");
+button112.addEventListener("click", function() {addtolist("112")});
+
+const button113 = document.getElementById("113");
+button113.addEventListener("click", function() {addtolist("113")});
+
+const button114 = document.getElementById("114");
+button114.addEventListener("click", function() {addtolist("114")});
+
+const button115 = document.getElementById("115");
+button115.addEventListener("click", function() {addtolist("115")});
+
+const button116 = document.getElementById("116");
+button116.addEventListener("click", function() {addtolist("116")});
+
+const button117 = document.getElementById("117");
+button117.addEventListener("click", function() {addtolist("117")});
+
+const button118 = document.getElementById("118");
+button118.addEventListener("click", function() {addtolist("118")});
+
+const button119 = document.getElementById("119");
+button119.addEventListener("click", function() {addtolist("119")});
+
+const button120 = document.getElementById("120");
+button120.addEventListener("click", function() {addtolist("120")});
+
+const button121 = document.getElementById("121");
+button121.addEventListener("click", function() {addtolist("121")});
+
+const button122 = document.getElementById("122");
+button122.addEventListener("click", function() {addtolist("122")});
+
+const button123 = document.getElementById("123");
+button123.addEventListener("click", function() {addtolist("123")});
+
+const button124 = document.getElementById("124");
+button124.addEventListener("click", function() {addtolist("124")});
+
+const button125 = document.getElementById("125");
+button125.addEventListener("click", function() {addtolist("125")});
+
+const button126 = document.getElementById("126");
+button126.addEventListener("click", function() {addtolist("126")});
+
+const button127 = document.getElementById("127");
+button127.addEventListener("click", function() {addtolist("127")});
+
+const button128 = document.getElementById("128");
+button128.addEventListener("click", function() {addtolist("128")});
+
+const button129 = document.getElementById("129");
+button129.addEventListener("click", function() {addtolist("129")});
+
+const button130 = document.getElementById("130");
+button130.addEventListener("click", function() {addtolist("130")});
+
+const button131 = document.getElementById("131");
+button131.addEventListener("click", function() {addtolist("131")});
+
+document.documentElement.style.setProperty("--box-shadow-color", "#9eb4cc");
+
+function addtolist(id) {
+    var index = symptoms.indexOf(id);
+    if (symptoms.includes(id) == false) {
+        symptoms.push(id);
+        document.getElementById(id).style.backgroundColor = "#F3E095";
+        document.getElementById(id).style.color = "#101e1e";
+        document.getElementById(id).style.setProperty("--box-shadow-color", "#F3E095");
+    }
+    else {
+        symptoms.splice(index, 1);
+        document.getElementById(id).style.backgroundColor = "#9eb4cc";
+        document.getElementById(id).style.color = "#101e1e";
+        document.getElementById(id).style.setProperty("--box-shadow-color", "#9eb4cc");
+    }
+    document.getElementById("result_text").innerHTML = "Waiting for symptoms...";
+}
+
+document.getElementById('go').addEventListener('click', function() {
+    if (symptoms.length > 0) {
+        var data = symptoms;
+        for (var i of symptoms) {
+            document.getElementById(i).style.backgroundColor = "#9eb4cc";
+            document.getElementById(i).style.color = "#101e1e";
+            document.getElementById(i).style.setProperty("--box-shadow-color", "#9eb4cc");
+        }
+        document.getElementById("result_text").innerHTML = "Loading...";
+
+        symptoms = [];
+        fetch('http://127.0.0.1:5000/api', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(function(response) {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(function(result) {
+          var result_array = result["identified_disease"];
+          if (result_array.length < 4) {
+            var prediction = result_array.join(', ');
+          }
+          else {
+            var prediction = "";
+            for (i of result_array) {
+              prediction = prediction + i
+            }
+          }
+          console.log('Response:', prediction);
+          document.getElementById("result_text").innerHTML = "Predicted Disease(s): " + prediction;
+        })
+        .catch(function(error) {
+          console.error('Error:', error);
+          document.getElementById("result_text").innerHTML = "Error! Try Later";
+        });
+    }
+});
